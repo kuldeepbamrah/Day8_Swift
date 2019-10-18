110661d106b90d7077c31584bf2cb19b56fd3d70
kuldeepbamrah <boby.sbamrah@gmail.com>
2019-10-18 18:32:55 -0400

    initial commit'

---
+++ Day8_Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day8_Swift
+//
+//  Created by MacStudent on 2019-10-18.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
